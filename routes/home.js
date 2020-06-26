ae91882740ff279220393bb3b94874c9493b4369
Turngait <wulzin.t@gmail.com>
2020-06-26 11:55:04 +0300

    Email

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,9 @@
 const { Router } = require('express')
+const {API_MAIL} = require('../config/api')
 const User = require('../models/user_model')
 const sequelize = require('../mysql/index')
+// const nodemailer = require('nodemailer')
+// const sendgrid = require('nodemailer-sendgrid-transport')
 const router = Router()
 
 router.get('/', async (req, res) => {
@@ -9,6 +12,16 @@ router.get('/', async (req, res) => {
     console.log(users)
 
     await sequelize.sync()
+
+    const transporter = nodemailer.createTransport(sendgrid({
+      auth: {api_key: API_MAIL}
+    }))
+    // transporter.sendMail({
+    //   to: 'ryzhov.is@mail.ru',
+    //   from: 'MyNotes<info@fraktur.ru>',
+    //   subject: 'Тестовое письмо с ноды',
+    //   html: '<h1>Привет!</h1><p>Это тестовое письмо с ноды!</p>'
+    // })
     res.send('MyNotes API')
   } catch (e) {
     console.log(e)
